8be406987a1118409cfa0570e8470ae07ec33452
kenan-would <kenan.wood@gmail.com>
2017-12-12 03:09:20 +1000

    addsliders

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 
 var boxcolor = 0;
 var eSize = 20;
-var biggerButton, smallerButton, currentSize;
+var biggerButton, smallerButton, currentSize, rSlider, gSlider, bSlider;
 
-function setup() {
+function setup() {P-
 	createCanvas(800, 400);
   createP('');
   biggerButton = createButton("+");
@@ -11,6 +11,10 @@ function setup() {
   currentSize = createElement('h1',eSize);
   biggerButton.mousePressed(increaseSize);
   smallerButton.mousePressed(decreaseSize);
+  rSlider = createSlider(0,0,100); 
+  gSlider = createSlider(0,0,100);
+  bSlider = createSlider(0,0,100);
+  
 }
 
 function draw() {
@@ -18,15 +22,21 @@ function draw() {
   // rectangle
   fill(mouseX,0,mouseY);
   ellipse(mouseX, mouseY, eSize);
+  fill(255);
+  textAlign(CENTER);
+  textSize(40);
+  text(eSize,40,40);
 }
+
 
   // mouse pressed
   function mousePressed() {
      background(0);
   // text
-  fill(115,0,113);
+  fill(rSlider.input(),gSlider.input(),bSlider.input());
   textSize(100);
-	text("WOW i Love VIM", 200, random(0,400));
+  
+  text("WOW i Love VIM", 20, random(0,400));
 }
 
 function increaseSize() {
